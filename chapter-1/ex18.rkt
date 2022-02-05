94cc3f3072960dc5dae7498e8bc30621d5b33e06
Jarosław Kołosowski <jarokolosowski@gmail.com>
2022-02-05 14:50:47 +0100

    Exercise 1.8

---
+++ chapter-1/ex18.rkt
@@ -0,0 +1,22 @@
+#lang racket
+
+(define (cbrt x)
+  (cbrt-iter 1.0 x))
+
+(define (cbrt-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (cbrt-iter (improve guess x) x)))
+
+(define (improve guess x)
+  (/ (+ (/ x (square guess)) (* 2 guess))
+     3))
+
+(define (good-enough? guess x)
+  (< (abs (- (* guess guess guess) x)) 0.001))
+
+(define (square x)
+  (* x x))
+
+(cbrt 8)
+(cbrt 27)
